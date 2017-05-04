d52ab4d01158a9dcdf7b2aadd6bb5eec60db3a86
maobon <maobonhaha@gmail.com>
2017-05-05 00:23:27 +0800

    Radio类型的input 和 select option标签的 默认选中处理 bootstrap-datepicker在react下工作有问题 待解决

--- src/components/TeacherEdt/TeacherEdt.js
+++ src/components/TeacherEdt/TeacherEdt.js
@@ -18,11 +18,12 @@ class TeacherEdt extends React.Component {
         this.state = {
             title: '',
             data: {
-                tc_gender: '',
+                tc_gender: '0',
                 tc_id: '',
                 tc_join_date: '',
                 tc_name: '',
-                tc_type: ''
+                tc_type: '1',
+                tc_pass: ''
             }
         }
     }
@@ -45,6 +46,9 @@ class TeacherEdt extends React.Component {
                     }
                 }
             });
+        } else {
+            this.state.title = '添加讲师'
+            this.setState({})
         }
     }
 
@@ -61,9 +65,17 @@ class TeacherEdt extends React.Component {
                 <div className="form-group">
                     <label className="col-xs-3 control-label">密码</label>
                     <div className="col-xs-4">
+
+                        {/* tc_pass */}
                         <input data-required data-pattern='^\d{6}$'
                                data-description='tcPass' data-describedby='tcPassInfo'
-                               name="tc_pass" type="password" className="form-control input-sm"/>
+                               name="tc_pass" type="password" className="form-control input-sm"
+                               onChange={(event) => {
+                                   this.state.data.tc_pass = event.target.value
+                                   this.setState({})
+                               }}
+                        />
+
                     </div>
                     <div className="col-xs-2" id="tcPassInfo"></div>
                 </div>
@@ -81,11 +93,10 @@ class TeacherEdt extends React.Component {
                             <li className="active">{ this.state.title }</li>
                         </ol>
 
-                        {/* 内容 */}
+                        {/* form表单 */}
                         <div className="teacher-add">
                             <form id="teacherInfoForm" className="form-horizontal col-xs-offset-2">
 
-                                {/* 讲师姓名 tc_name */}
                                 <div className="form-group">
                                     <label className="col-xs-3 control-label">姓名</label>
                                     <div className="col-xs-4">
@@ -93,6 +104,7 @@ class TeacherEdt extends React.Component {
                                         {/* 讲师id tc_id 使用隐藏域携带 */}
                                         {tc_id_hidden}
 
+                                        {/* 讲师姓名 tc_name */}
                                         <input data-required data-description="tcName" data-describedby="tcNameInfo"
                                                name="tc_name"
                                                value={ this.state.data.tc_name }
@@ -104,54 +116,59 @@ class TeacherEdt extends React.Component {
                                     <div className="col-xs-2" id="tcNameInfo"></div>
                                 </div>
 
-                                {/* 登录密码 tc_pass 添加讲师时展示 */}
+                                {/* 登录密码 tc_pass 添加讲师时展示(编辑讲师信息时不展示) */}
                                 {tc_pass_dom}
 
-                                {/* 入职时间 tc_join_date */}
                                 <div className="form-group">
                                     <label className="col-xs-3 control-label">入职时间</label>
                                     <div className="col-xs-4">
+
+                                        {/* 入职时间 tc_join_date (bootstrap插件) */}
                                         <input data-required
                                                data-description="joinDate" data-describedby="tcJoinDateInfo"
                                                name="tc_join_date"
                                                value={ this.state.data.tc_join_date }
-                                               onChange={(event) => {
-                                                   this.state.data.tc_join_date = event.target.value;
-                                                   this.setState({})
-                                               }}
                                                data-date-language="zh-CN" data-provide="datepicker"
                                                data-date-format="yyyy-mm-dd"
                                                data-date-end-date="0d"
-                                               type="text" className="form-control input-sm"/>
+                                               type="text" className="form-control input-sm"
+                                               changeDate={console.log(111111)}
+                                               changeYear={console.log(222222)}
+                                               changeMonth={console.log(333333)}
+                                        />
+
                                     </div>
                                     <div className="col-xs-2" id="tcJoinDateInfo"></div>
                                 </div>
 
-                                {/* 用户类型 tc_type 管理员or讲师 */}
                                 <div className="form-group">
                                     <label className="col-xs-3 control-label">类型</label>
                                     <div className="col-xs-2">
+
+                                        {/* 用户类型 tc_type 管理员or讲师 */}
                                         <select name="tc_type" className="form-control input-sm"
                                                 value={this.state.data.tc_type}
                                                 onChange={(event) => {
-                                                    this.state.data.tc_type = event.target.value;
+                                                    this.state.data.tc_type = event.target.value
                                                     this.setState({})
                                                 }}>
                                             <option value="1">讲师</option>
                                             <option value="0">管理员</option>
                                         </select>
+
                                     </div>
                                 </div>
 
-                                {/* 性别 tc_gender */}
                                 <div className="form-group">
                                     <label className="col-xs-3 control-label">性别</label>
                                     <div className="col-xs-4">
+
+                                        {/* 性别 tc_gender */}
                                         <label className="radio-inline">
                                             <input type="radio" name="tc_gender" value="0"
                                                    checked={this.state.data.tc_gender == '0'}
                                                    onChange={(event) => {
-                                                       this.state.data.tc_gender = event.target.value;
+                                                       this.state.data.tc_gender = event.target.value
                                                        this.setState({})
                                                    }}/> 男
                                         </label>
@@ -159,17 +176,17 @@ class TeacherEdt extends React.Component {
                                             <input type="radio" name="tc_gender" value="1"
                                                    checked={this.state.data.tc_gender == '1'}
                                                    onChange={(event) => {
-                                                       this.state.data.tc_gender = event.target.value;
+                                                       this.state.data.tc_gender = event.target.value
                                                        this.setState({})
                                                    }}/> 女
                                         </label>
                                     </div>
-
                                 </div>
 
-                                {/* 提交修改信息 */}
                                 <div className="form-group">
                                     <div className="col-xs-7">
+
+                                        {/* SUBMIT BUTTON */}
                                         <input type="submit" value="提 交"
                                                className="btn btn-success btn-sm pull-right btn-submit"
                                                onClick={(e) => {
@@ -177,6 +194,7 @@ class TeacherEdt extends React.Component {
                                                    this.doSubmit()
                                                }}
                                         />
+
                                     </div>
                                 </div>
 
@@ -191,6 +209,28 @@ class TeacherEdt extends React.Component {
 
     doSubmit() {
         console.log(this.state.data)
+
+        // if (this.state.title == '编辑讲师') {
+        //     console.log('bianji')
+        //
+        // } else if (this.state.title == '添加讲师') {
+        //     console.log('tianjia')
+        //
+        //     let params = this.state.data
+        //
+        //     $.ajax({
+        //         type: 'post',
+        //         url: '/api/teacher/add',
+        //         dataType: 'json',
+        //         data: params,
+        //         success: function (res) {
+        //             console.log(res)
+        //         },
+        //         error: function (err) {
+        //             console.log(err)
+        //         }
+        //     })
+        // }
     }
 }
 
